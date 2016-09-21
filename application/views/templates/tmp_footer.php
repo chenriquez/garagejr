c9c8c8081b88a862578572c7f45b216c052b4fad
= <=>
2016-09-21 17:58:52 -0300

    implementacion e inicio con codeigniter

---
+++ application/views/templates/tmp_footer.php
@@ -0,0 +1,25 @@
+    <div class="container">
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Garaje-JR. Todos los derechos reservados.</p>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <!-- /.container    
+    <!-- jQuery -->
+    <script src="<?php echo base_url('assets/js/jquery-2.1.4.min.js'); ?>"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+        crossorigin="anonymous">
+    </script>
+
+    <!-- Script to Activate the Carousel -->
+    
+
+</body>
+</html>
